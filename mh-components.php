a9c998704962140fc5e2d121291fe2b7d6e61db3
Matthias Hauch <matthias.hauch@gmx.net>
2016-04-20 22:23:50 +0200

    create own plugin file for testing

---
+++ mh-components.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name:       MH Components
+ * Plugin URI:        http://getonepager.com
+ * Description:       Based on OnePager (http://getonepager.com) with improvements. Divides a Wordpress page in components (blocks).
+ * Version:           1.2.5
+ * Author:            Matthias Hauch
+ * Author URI:        http://www.matthias-draeger.info
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       mh-onepager
+ * Domain Path:       /languages
+ * GitHub Plugin URI: https://github.com/matti142/mh-components
+ * GitHub Branch:     improvements
+ */
+
+// using main onepager file
+require( __DIR__ . "/tx-onepager.php" );
